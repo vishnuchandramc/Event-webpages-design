24a8016c94d4183724ace16a018dfc785edc1c6a
Vishnuchandra.mc <43258677+vishnuchandramc@users.noreply.github.com>
2021-06-07 20:55:53 +0530

    Add files via upload

---
+++ Event_website/Website/resources/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('.scroll').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+   /* Scroll on buttons */
+    $('.js--scroll-to-register').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+     });
+     
+     $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+     });
+      $(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+           var target = $(this.hash);
+           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+               scrollTop: target.offset().top
+             }, 1000);
+             return false;
+           }
+         }
+       });
+     });
+     $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated slideInLeft');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated slideInRight');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated flipInX');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    });
